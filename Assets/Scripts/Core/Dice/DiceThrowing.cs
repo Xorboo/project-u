9eb9557ca0861c362aaa020184d52eab42e5f66e
Yaroslav Kravtsov <yaroslav.kravtsov@gmail.com>
2022-07-16 13:12:33 +0300

    Прототип кидания кубика + шрифты + декоративные правки префабов
    Пока кубик кидается в отдельной сцене, но под это дело я уже завел слои и настройки физики в проекте

---
+++ Assets/Scripts/Core/Dice/DiceThrowing.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceThrowing : MonoBehaviour
+{
+    bool following_mouse = false;
+    
+
+    [SerializeField] LayerMask layerMask_take, layerMask_throw;
+    Rigidbody _body;
+    Vector3 rotating_direction;
+    [SerializeField] float throwing_force = 3f;
+
+    bool throwing = false;
+    Vector3 rotation_angles_old;
+    int frames_count = 0;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !throwing)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);            
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, layerMask_take))
+            {
+                if (hit.transform.name == "Dice")
+                {
+                    following_mouse = true;
+                    rotating_direction = new Vector3(Random.Range(1, 3), Random.Range(1, 3), Random.Range(1, 3)); //Каждый бросок кубик будет крутиться в случайном направлении, чтобы игрок не приноровился кидать шестерки
+                }                
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            following_mouse = false;
+            throwing = true;
+            rotation_angles_old = transform.rotation.eulerAngles;
+            frames_count = 0;
+        }
+
+        if (following_mouse)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, layerMask_throw))
+            {
+                Vector3 mouse_position = hit.point;
+                //_body.AddForce((mouse_position - transform.position) * Mathf.Lerp(0, throwing_force, dist / 10f));
+                _body.velocity = (mouse_position - transform.position) * throwing_force;
+                //transform.position = hit.point;                
+                _body.AddTorque(rotating_direction);
+            }
+        }
+
+        if (throwing)
+        {
+            Vector3 rotation_angles_new = transform.rotation.eulerAngles;
+            if (rotation_angles_new == rotation_angles_old)
+            {
+                frames_count++;
+                if (frames_count > 30)
+                {
+                    Debug.Log("End");
+                    throwing = false;
+                    ThrowingResult();
+                }
+            }
+            else
+            {
+                frames_count = 0;
+            }
+            rotation_angles_old = rotation_angles_new;
+        }
+    }
+
+    void ThrowingResult()
+    {
+        float x = Mathf.Round(transform.rotation.eulerAngles.x);
+        float z = Mathf.Round(transform.rotation.eulerAngles.z);
+        int dice_value = 0;
+        //print(x + "  " + z);
+        if (z == 0)
+            dice_value = 1;
+        if (z == 270)
+            dice_value = 2;
+        if (x == 270)
+            dice_value = 3;
+        if (x == 90)
+            dice_value = 4;
+        if (z == 90)
+            dice_value = 5;
+        if (z == 180)
+            dice_value = 6;
+
+        print(dice_value);
+    }
+}
